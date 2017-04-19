a3b6fab24806f18234c245ac72209b906c351c47
Adam K. Hastings <adam@CB461-EE10030.ee.byu.edu>
2017-04-18 23:40:15 -0600

    added get_distinct_beat, get_pitch_variance, and used the fft to find principle frequency and signal-to-noise ratio

--- instrument.py
+++ instrument.py
@@ -1,4 +1,9 @@
 import numpy as np
+import numpy.fft as fft
+import matplotlib.pyplot as plt
+import math
+from math import sin, pi, log
+from random import randint
 from collections import defaultdict
 
 
@@ -18,6 +23,12 @@ class Instrument:
         self.melodic = None
         self.related = []
         self.last_pos, self.last_note = 0, None
+
+        # Adam's added code
+        self.unique_pitches = 0
+        self.beat_occurrences = 0
+        self.snr = 0
+        self.principle_freq = 0
 
     def add_note(self, note, pos):
         # Record data
@@ -49,20 +60,79 @@ class Instrument:
 
     def analyze_pitch(self):
         if not self.sample or not self.sample._wave:
-            raise Exception('No wave to analyze')
-        fft = np.fft.rfft(self.wave)
-        np.abs(fft, fft)
-        np.square(fft, fft)
-        np.log(fft, fft)
-        dom_freq_dex = np.argmax(fft)
+           raise Exception('No wave to analyze')
+
+        myWave = [100 * sin(2*pi*n/32) for n in range(32)]
+        myWave = np.array(myWave)
+
+        # Test case for computing snr
+        # myWave = [randint(0,100000) for n in range(100)]
+
+
+        # fft = np.fft.rfft(myWave)
+        # np.abs(fft, fft)
+        # np.square(fft, fft)
+        # np.log(fft, fft)
+        # dom_freq_dex = np.argmax(fft)
+
         # Still needs interpretation, check for off-key notes, and turn
         # this into something that can tell us basically what octave
         # the instrument is in.
+
+        spectrum = np.fft.fft(myWave)
+        freqs = np.fft.fftfreq(len(spectrum))
+
+        idx = np.argmax(np.abs(spectrum))
+        freq = freqs[idx]
+        sample_rate = 100 # TODO
+        freq_in_hertz = abs(freq * sample_rate)
+        plt.plot(freqs, abs(spectrum))
+
+
+        # Following what I read here: https://dsp.stackexchange.com/questions/14862/measure-the-snr-of-a-signal-in-the-frequency-domain
+        # And here: https://en.wikipedia.org/wiki/Signal-to-noise_ratio#Definition
+
+        abs_spectrum = abs(spectrum)
+
+
+        max_power = 0
+        for i in range(1,math.floor(len(abs_spectrum)/2)):
+            if abs_spectrum[i] > max_power:
+                max_power = abs_spectrum[i]
+
+        P_n = 0
+        for i in range(1,math.floor(len(abs_spectrum)/2)):
+            if (abs_spectrum[i] < max_power/2):
+                P_n += abs_spectrum[i] * abs_spectrum[i]
+        P_n /= len(abs_spectrum)
+
+        P_s = 0
+        for i in range(1,math.floor(len(abs_spectrum)/2)):
+            if (abs_spectrum[i] > max_power/2):
+                P_s += abs_spectrum[i] * abs_spectrum[i]
+        P_s /= len(abs_spectrum)
+
+        snr = P_s / P_n
+        print("snr: " + str(snr))
+
+        # Signal to noise ratio defined as mean / std deviation # of original signal, not the fft.............
+        # snr = np.mean(myWave)/np.std(noise_list)
+        # print(snr)
 
         # Then save another parameter that's something like the magnitude
         # of the dominant pitch fft[dom_freq_dex] divided by the mean of
         # the rest of it or something, so that a bass drum with a strong
         # but not tonal peak will be distinct from a bass guitar with the
         # same peak location and magnitude (but more prominent, higher SNR)
-        raise NotImplementedError('Still have to write analyze_pitch')
+        # raise NotImplementedError('Still have to write analyze_pitch')
+
+        self.principle_freq = freq_in_hertz
+        self.snr = snr
         
+    def get_distinct_beat(self):
+        self.beat_occurrences = len(self.beats)
+
+    def get_pitch_variance(self):
+        self.unique_pitches = len(self.pitches)
+
+Instrument.analyze_pitch()
