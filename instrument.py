477548e027a0c87b8abb30d50ab39ce0074495e8
Adam K. Hastings <adam@CB461-EE10030.ee.byu.edu>
2017-04-19 13:36:09 -0600

    added get_avg_pitch; now using actual sample and correct (?) sample rate to determine an instrument's frequency.

--- instrument.py
+++ instrument.py
@@ -22,6 +22,29 @@ FREQ2MIDI = {
 
 freq_array = [130.8, 261.6, 392.0, 138.6, 523.3, 784.0, 146.8, 659.3, 622.3, 1046.5, 155.6, 329.6, 415.3, 1568.0, 164.8, 293.7, 1318.5, 220.0, 554.4, 174.6, 466.2, 1864.7, 1975.5, 311.1, 440.0, 185.0, 698.5, 830.6, 65.4, 196.0, 69.3, 1244.5, 1480.0, 73.4, 587.3, 77.8, 207.7, 82.4, 1108.7, 87.3, 932.3, 987.8, 92.5, 349.2, 1760.0, 98.0, 740.0, 103.8, 233.1, 493.9, 110.0, 1396.9, 880.0, 370.0, 116.5, 246.9, 1174.7, 123.5, 1661.2, 277.2]
 
+# MIDI ti MOD pitch numbers
+MIDI2MODPITCHES={
+    0:1712, 1:1616, 2:1525, 3:1440, 4:1357, 5:1281, 6:1209, 7:1141,
+    8:1077, 9:1017, 10:961, 11:907, 12:856, 13:808, 14:762, 15:720,
+    16:678, 17:640, 18:604, 19:570, 20:538, 21:508, 22:480, 23:453,
+    24:428, 25:404, 26:381, 27:360, 28:339, 29:320, 30:302, 31:285,
+    32:269, 33:254, 34:240, 35:226, 36:214, 37:202, 38:190, 39:180,
+    40:170, 41:160, 42:151, 43:143, 44:135, 45:127, 46:120, 47:113,
+    48:107, 49:101, 50:95, 51:90, 52:85, 53:80, 54:76, 55:71, 56:67,
+    57:64, 58:60, 59:57}
+
+NAMES2MIDI = {
+    'C-0': 0, 'C#0': 1, 'D-0': 2, 'D#0': 3, 'E-0': 4, 'F-0': 5,
+    'F#0': 6, 'G-0': 7, 'G#0': 8, 'A-0': 9, 'A#0': 10, 'B-0': 11,
+    'C-1': 12, 'C#1': 13, 'D-1': 14, 'D#1': 15, 'E-1': 16, 'F-1': 17,
+    'F#1': 18, 'G-1': 19, 'G#1': 20, 'A-1': 21, 'A#1': 22, 'B-1': 23,
+    'C-2': 24, 'C#2': 25, 'D-2': 26, 'D#2': 27, 'E-2': 28, 'F-2': 29,
+    'F#2': 30, 'G-2': 31, 'G#2': 32, 'A-2': 33, 'A#2': 34, 'B-2': 35,
+    'C-3': 36, 'C#3': 37, 'D-3': 38, 'D#3': 39, 'E-3': 40, 'F-3': 41,
+    'F#3': 42, 'G-3': 43, 'G#3': 44, 'A-3': 45, 'A#3': 46, 'B-3': 47,
+    'C-4': 48, 'C#4': 49, 'D-4': 50, 'D#4': 51, 'E-4': 52, 'F-4': 53,
+    'F#4': 54, 'G-4': 55, 'G#4': 56, 'A-4': 57, 'A#4': 58, 'B-4': 59 }
+
 class Instrument:
 
     BEATS = 16  # Length of beat sequences to track (max 64)
@@ -45,6 +68,20 @@ class Instrument:
         self.beat_occurrences = 0
         self.snr = 0
         self.principle_freq = 0
+
+        self.rounded_pitch_num = 0
+
+    def get_avg_pitch(self):
+        pitch_sum = 0
+        for key, value in self.pitches.items():
+            pitch_sum += (value * NAMES2MIDI[key])
+        avg_midi_pitch = pitch_sum/len(self.pitches)
+        pitch_nums = list(range(60))
+
+        idx = (np.abs(pitch_nums-avg_midi_pitch)).argmin()
+        self.rounded_pitch_num =  pitch_nums[idx] 
+
+
 
     def add_note(self, note, pos):
         # Record data
@@ -82,8 +119,8 @@ class Instrument:
         if not self.sample or not self.sample._wave:
           raise Exception('No wave to analyze')
 
-        myWave = [100 * sin(2*pi*n/32) for n in range(32)]
-        myWave = np.array(myWave)
+        #myWave = [100 * sin(2*pi*n/32) for n in range(32)]
+        #myWave = np.array(myWave)
 
         # Test case for computing snr
         # myWave = [randint(0,100000) for n in range(100)]
@@ -99,12 +136,12 @@ class Instrument:
         # this into something that can tell us basically what octave
         # the instrument is in.
 
-        spectrum = np.fft.fft(myWave)
+        spectrum = np.fft.fft(self.sample)
         freqs = np.fft.fftfreq(len(spectrum))
 
         idx = np.argmax(np.abs(spectrum))
         freq = freqs[idx]
-        sample_rate = 100 # TODO
+        sample_rate = self.rounded_pitch_num # TODO
         freq_in_hertz = abs(freq * sample_rate)
         print("freq in hertz: " + str(freq_in_hertz))
         plt.plot(freqs, abs(spectrum))
